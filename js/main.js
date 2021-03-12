f407ae777b8b4c302cf2fb8791e35056edc4ee9b
Julian Nuñez <69222010+njulian18@users.noreply.github.com>
2021-03-12 13:01:12 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function cambiarColor(){
+    document.getElementById("header").style.backgroundColor = "#294936"; 
+    document.getElementById("nav").style.backgroundColor = "#30343F";
+    document.getElementById("section").style.backgroundColor = "#5B8266";
+    document.getElementById("aside").style.backgroundColor = "#1A5C94";
+    document.getElementById("footer").style.backgroundColor = "#294936";
+  
+}
+
+function colorBase(){
+    document.getElementById("header").style.backgroundColor = "#30343F"; 
+    document.getElementById("nav").style.backgroundColor = "#294936";
+    document.getElementById("section").style.backgroundColor = "#3E6259";
+    document.getElementById("aside").style.backgroundColor = "#5B8266";
+    document.getElementById("footer").style.backgroundColor = "#30343F";
+   
+}
+
+ 
